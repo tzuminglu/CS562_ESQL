1ba15193a0ed420709e545fb50ac1ee20282075f
TonyShi12345 <asd19970623@163.com>
2023-12-20 17:19:25 -0500

    update

--- coreProcess/mf_Structure.py
+++ coreProcess/mf_Structure.py
@@ -2,16 +2,16 @@ def mf_Structure(S,F,G):
     mf_structure={}
     for s in S:
         if s not in mf_structure:
-            mf_structure[s]=[]
+            mf_structure[s]=None
     for f in F:
         if f not in mf_structure:
-            mf_structure[f]=[]
+            mf_structure[f]=None
     if (G) and (G[0]):
         array=G[0].split()
         for name in array:
             if "." in name or "_" in name:
                 if s not in mf_structure:
-                    mf_structure[name]=[]
+                    mf_structure[name]=None
     print(f"md_structure: {mf_structure}")
     return mf_structure
     
